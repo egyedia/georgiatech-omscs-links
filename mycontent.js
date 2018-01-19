8bd0b0c8c79eb216ac3e95ad65867cb4eef682cc
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-01-18 16:23:14 -0800

    Added new resources

--- mycontent.js
+++ mycontent.js
@@ -6,14 +6,34 @@ var myContent = [
             "https://buzzport.gatech.edu/": "BuzzPort",
             "https://registrar.gatech.edu/calendar?view=table": "Academic Calendar",
             "http://www.omscs.gatech.edu/current-courses": "Current Courses",
+            "https://canvas.gatech.edu/": "Canvas",
+            "https://gatech.instructure.com/": "Canvas Login",
             "https://t-square.gatech.edu/": "T-Square",
             "http://piazza.com/": "Piazza",
             "https://passport.gatech.edu/": "Passport",
             "http://mail.gatech.edu/": "Webmail - Office 365 Outlook",
             "https://testing.verificient.com/": "Test - Proctor Track",
             "https://oscar.gatech.edu/": "OSCAR",
+            "http://bursar.gatech.edu/": "Bursar",
             "https://docs.google.com/document/d/1ZtWIxQht7OSfB0ey--U3pvI4pisfZsKo_usvLzlXBQo/": "OMSCS FAQ",
             "https://www.youtube.com/watch?v=wEGkk6L7yq0&list=PLtYJ8HXD0d7BRjUZSYJhLz5_nLX5GvXRG&index=1": "Youtube: Welcome to Georgia Tech!"
+        }
+    },
+    {
+        "title": "Georgia Tech Bursar",
+        "cssClass": "gatech",
+        "links": {
+            "http://bursar.gatech.edu/": "Bursar",
+            "mailto:bursar.ask@business.gatech.edu": "bursar.ask@business.gatech.edu",
+            "tel:+1 (404) 894 4618": "+1 (404) 894 4618",
+        }
+    },
+    {
+        "title": "Georgia Tech Third Party Billing",
+        "cssClass": "gatech",
+        "links": {
+            "http://www.bursar.gatech.edu/content/third-party-billing": "Third party billing",
+            "mailto:thirdpartybilling@gatech.edu": "thirdpartybilling@gatech.edu",
         }
     },
     {
@@ -34,12 +54,13 @@ var myContent = [
         "title": "6750 - Human-Computer Interaction",
         "cssClass": "class",
         "links": {
+            "http://omscs6750.gatech.edu/spring-2018/required-reading-list/": "Required Reading List",
+            "http://omscs6750.gatech.edu/spring-2018/full-calendar/": "Spring 2018 Course Calendar",
+            "https://classroom.udacity.com/courses/ud400": "Udacity",
             "https://piazza.com/class/jbu1v11roji5d4": "Piazza",
             "https://gatech.instructure.com/courses/9591": "Canvas",
             "http://omscs6750.gatech.edu/spring-2018/": "Course own website",
             "https://gatech.instructure.com/courses/9591/assignments/syllabus": "Syllabus on Canvas",
-            "http://omscs6750.gatech.edu/spring-2018/full-calendar/": "Course Calendar",
-            "https://classroom.udacity.com/courses/ud400": "Udacity",
             "https://t-square.gatech.edu/portal/site/gtc-3ed6-d2a6-5e03-b1d4-fcd4136596b4": "T-Square - not used"
         }
     },
